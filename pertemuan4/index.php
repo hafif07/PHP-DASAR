312dfdb60df6c3dc1b9ffc62c323f150d67b6e2b
hafif07 <mhafif29@gmail.com>
2020-01-08 16:36:05 +0700

    add topic2

--- pertemuan4/index.php
+++ pertemuan4/index.php
@@ -23,6 +23,47 @@ echo "hari ulang tahunku adalah hari = ";
 // mktime(0,0,0,0,0,0)
 // jam , menit , detik , bulan, tanggal , tahun
 
-echo date("l",mktime(0,0,0,12,03,1999))
+echo date("l",mktime(0,0,0,12,03,1999));
 
- ?>
+$pc_name = "Hafif";
+$waktu_kategori="malam";
+
+function salam($waktu,$nama){
+	return "selamat $waktu, $nama !";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ducument</title>
+	<style type="text/css">
+		.pagi{
+			background-color: aqua
+		}
+		.sore{
+			background-color: orange
+		}
+		.malam{
+			background-color: black;
+			color:white;
+		}
+	</style>
+</head>
+<?php if($waktu_kategori=="pagi"): ?>
+	<body class = "pagi">
+		<h1><?php echo salam($waktu_kategori,$pc_name) ?></h1>
+<?php elseif($waktu_kategori=="sore"):?>
+	<body class = "sore">
+		<h1><?php echo salam($waktu_kategori,$pc_name) ?></h1>
+<?php else: ?>
+	<body class = "malam">
+		<h1><?php echo salam($waktu_kategori,$pc_name) ?></h1>
+<?php endif ;?>
+	</body>
+</html>
+
+
+
+
---
+++ pertemuan5/index.php
@@ -0,0 +1,3 @@
+<?php 
+echo "pertemuan 5"
+ ?>
